b125d19a3db6d75d0713951729b00d1597982763
Samarth Bhardwaj <85241456+Samarth-5109@users.noreply.github.com>
2021-09-30 17:47:01 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+var symbolDictionary = {
+  "⛔": "No Entry",
+  "🚫": "Prohibited",
+  "🚳": "No Bicycles",
+  "🚭": "No Smoking",
+  "🚯": "No Littering",
+  "🚱": "Non-Potable Water",
+  "🚷": "No Pedestrians",
+  "📵": "No Mobile Phones",
+  "☢️": "Radioactive",
+  "☣️": "Biohazard"
+};
+
+var emojiWeKnew = Object.keys(symbolDictionary);
+
+export default function App() {
+  var [meaning, setMeaning] = useState("");
+
+  function emojiInputHandler(event) {
+    var userInput = event.target.value;
+    var meaning = symbolDictionary[userInput];
+    setMeaning(meaning);
+  }
+  function emojiClickHandler(emoji) {
+    var meaning = symbolDictionary[emoji];
+    setMeaning(meaning);
+  }
+
+  return (
+    <div className="App">
+      <h1>Symbol Interpreter</h1>
+      <input
+        placeholder="place your flag here"
+        onChange={emojiInputHandler}
+      ></input>
+      <div onChange={emojiInputHandler}>{meaning}</div>
+
+      <div></div>
+      {emojiWeKnew.map(function (emoji) {
+        return (
+          <span key={emoji} onClick={() => emojiClickHandler(emoji)}>
+            {emoji}
+          </span>
+        );
+      })}
+    </div>
+  );
+}
